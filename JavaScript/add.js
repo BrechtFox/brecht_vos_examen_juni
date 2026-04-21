0fc880bc57720e55cedcb3df03f2328c9e4182fa
BrechtFox <vosbrecht1@gmail.com>
2026-04-21 11:13:46 +0200

    main: basis functionaliteit werkt

--- JavaScript/add.js
+++ JavaScript/add.js
@@ -2,13 +2,52 @@ import { updateLS } from "./utils.js";
 
 let allSlotcars = JSON.parse(localStorage.getItem('savedSlotcars')) || [];
 
-document.querySelector('form.add').addEventListener('submit', (e) => {
+document.querySelector('form').addEventListener('submit', (e) => {
     e.preventDefault();
     add()
 });
 
-function add() {
-    const newSlotcar = {
-        
-    }
+function showMessage(type, message) {
+    document.querySelector('#message').innerHTML = `<p class="${type}">${message}</p>`
 }
+
+function add() {
+    // newId made by chatGPT
+    let newId = allSlotcars.length > 0
+    ? Math.max(...allSlotcars.map(item => item.id)) + 1
+    : 1;
+    const newSlotcar = {
+        maker: document.querySelector('#maker').value,
+        carClass: document.querySelector('#car-class').value,
+        brand: document.querySelector('#brand').value,
+        scale: document.querySelector('#scale').value,
+        color: document.querySelector('#color').value,
+        price: document.querySelector('#price').value,
+        state: document.querySelector('#state').value,
+        id: newId
+    }
+    if (newSlotcar.maker && newSlotcar.carClass && newSlotcar.brand && newSlotcar.scale && newSlotcar.color && newSlotcar.price && newSlotcar.state) {
+        if (["BRM", "ScaleAuto", "Gecko"].includes(newSlotcar.maker)) {
+            if (["minicar", "youngtimer", "GT pro"].includes(newSlotcar.carClass)) {
+                if (["mini", "viper", "porsche"].includes(newSlotcar.brand)) {
+                    if (["1:24", "1:32"].includes(newSlotcar.scale)) {
+                        
+                        allSlotcars.push(newSlotcar);
+                        updateLS(allSlotcars);
+                        showMessage("success", "Nieuwe slotcar toegevoegd.");
+                    } else {
+                        showMessage("error", "Schaal bestaat niet, nieuwe slotcar niet toegevoegd.");
+                    };
+                } else {
+                    showMessage("error", "Merk niet gevonden, nieuwe slotcar niet toegevoegd.");
+                };
+            } else {
+                showMessage("error", "Klasse niet gevonden, nieuwe slotcar niet toegevoegd.");
+            };
+        } else {
+            showMessage("error", "Maker niet gevonden, nieuwe slotcar niet toegevoegd.");
+        };
+    } else {
+        showMessage("error", "Niet alle velden ingevuld, nieuwe slotcar niet toegevoegd.");
+    };
+};
--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,3 +1,42 @@
 import { updateLS } from "./utils.js";
 
 let allSlotcars = JSON.parse(localStorage.getItem('savedSlotcars')) || [];
+
+function showSlotcars() {
+    const list = document.querySelector(".items");
+    if (allSlotcars.length === 0) {
+        list.innerHTML = '<p>Nog geen slotcars toegevoegd</p>';
+        return
+    }
+    list.innerHTML = allSlotcars.map(slot => `
+        <article id="${slot.id}" class="${slot.state}">
+                <div class="info">
+                    <h2 class="maker">${slot.maker}</h2>
+                    <p class="car-class">${slot.carClass}</p>
+                    <p class="brand">${slot.brand}</p>
+                    <p class="scale">${slot.scale}</p>
+                    <p class="color">${slot.color}</p>
+                    <p class="price">€${slot.price}</p>
+                </div>
+                <img src="img/${slot.brand}_logo.png" alt="${slot.brand} logo">
+                <button class="delete-btn" data-id="${slot.id}">❌</button>
+            </article>`
+    ).join('')
+    document.querySelectorAll(".delete-btn").forEach(button => {
+        button.addEventListener("click", deleteSlotcar);
+    });
+}
+
+function deleteSlotcar(e) {
+    const id = parseInt(e.target.dataset.id);
+    
+    allSlotcars = allSlotcars.filter(item => item.id !== Number(id))
+
+    // update localStorage
+    updateLS(allSlotcars);
+
+    // re-render
+    showSlotcars();
+}
+
+showSlotcars()
